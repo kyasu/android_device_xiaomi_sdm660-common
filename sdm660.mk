f0d3e8c450dde2c96f8b6bfe3d3f171c95301ff0
sabarop <sabar.pakpahan@gmail.com>
2022-10-01 22:24:56 +0700

    sdm660-common: Build Lineage Trust

--- sdm660.mk
+++ sdm660.mk
@@ -581,6 +581,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
     $(COMMON_PATH)/configs/thermal/thermal-engine-performance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-performance.conf
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti

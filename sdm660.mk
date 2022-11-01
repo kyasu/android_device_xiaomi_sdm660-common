d428d9652e86bf8a5d55c782bab35ffc7fdad04b
sabarop <sabar.pakpahan@gmail.com>
2022-11-01 11:28:29 +0700

    sdm660-common: Removes offending entries

--- sdm660.mk
+++ sdm660.mk
@@ -184,8 +184,6 @@ endif
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.graphics.allocator@3.0-impl \
-    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \

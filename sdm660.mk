29d659b0fe6c37330249333c97cce28d6d113a88
Sabar <31942277+sabarop@users.noreply.github.com>
2023-07-06 14:27:54 +0700

    sdm660-common: build more ril modules

--- sdm660.mk
+++ sdm660.mk
@@ -523,7 +523,10 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
+    qti-telephony-utils-prd \
     qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml \
+    rild \
     telephony-ext
 
 # RIL JAR
